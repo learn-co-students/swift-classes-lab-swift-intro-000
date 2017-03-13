b4f2a187e3b58d8aaa5a15cd68b42e744d080ff7
cmlndm <cmlndm@gmail.com>
2017-03-13 10:30:32 -0500

    Classes Lab

--- Thermometer/House.swift
+++ Thermometer/House.swift
@@ -8,5 +8,14 @@
 
 import Foundation
 
-
+class House {
+    
+    let thermometer: Thermometer = Thermometer(fahrenheit: 75.0)
+    
+    func grandmaIsOver() {
+        thermometer.fahrenheit = 90.0
+    }
+    
+    
+}
 
