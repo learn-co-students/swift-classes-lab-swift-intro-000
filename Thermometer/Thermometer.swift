8403cfc7c14d0953002527d82f14e03b597cea02
g3kenny <g3_kenny@hotmail.com>
2017-03-15 01:07:33 +0800

    commit class lab1

--- Thermometer/Thermometer.swift
+++ Thermometer/Thermometer.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-
+class Thermometer {
+    var fahrenheit: Double
+    var celsius: Double {
+        set{
+            fahrenheit = (newValue * (9 / 5)) + 32
+        }
+        get{
+            return (fahrenheit - 32) * (5 / 9)
+        }
+    }
+    var description: String {
+        return "Fahrenheit: \(self.fahrenheit)\nCelsius: \(self.celsius)"
+    }
+    
+    init (fahrenheit: Double) {
+        self.fahrenheit = fahrenheit
+    }
+}
 
