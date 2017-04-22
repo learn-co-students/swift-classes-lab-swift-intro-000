2de829252391c72d8fbc5d7555a033fcef4959b7
Kristof Buts <kr.buts@gmail.com>
2017-04-22 14:14:24 +0200

    done

--- Thermometer/Thermometer.swift
+++ Thermometer/Thermometer.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-
+class Thermometer {
+    var description: String {
+        var ret = ""
+        ret += "Fahrenheit: " + String(fahrenheit) + "\n"
+        ret += "Celsius: " + String(celsius)
+        return ret
+    }
+    var fahrenheit: Double
+    var celsius: Double {
+        get {
+            return ((fahrenheit-32)*(5/9))
+        }
+        set {
+            fahrenheit = (newValue*(9/5))+32
+        }
+    }
+    init(fahrenheit: Double) {
+        self.fahrenheit = fahrenheit
+    }
+}
 
