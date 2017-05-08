1436b74fe6dff756615ef7bcbe41d49a68893f39
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-05-08 22:22:27 +1200

    completed swift classes lab

--- Thermometer/Thermometer.swift
+++ Thermometer/Thermometer.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-
-
+class Thermometer {
+    
+    var fahrenheit: Double;
+    var description: String {
+        return "Fahrenheit: \(fahrenheit)\nCelsius: \(celsius)";
+    }
+    var celsius: Double {
+        get {
+            return (fahrenheit - 32) * 5/9;
+        }
+        set {
+            fahrenheit = (9/5 * newValue) + 32;
+        }
+    }
+    
+    init(fahrenheit: Double) {
+        self.fahrenheit = fahrenheit;
+    }
+    
+}
