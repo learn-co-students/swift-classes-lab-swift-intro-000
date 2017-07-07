faefef64951ef4b63a68c3bb51aa594f0aa6d341
Jacque DesMarais <jacquedes@gmail.com>
2017-07-06 20:44:56 -0500

    Solutions
    Solutions

--- Thermometer/House.swift
+++ Thermometer/House.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
+class House {
+    let thermometer: Thermometer = Thermometer(fahrenheit: 70.0)
+    
+    func grandmaIsOver() {
+        self.thermometer.fahrenheit = 90.0
+    }
+}
 
 
