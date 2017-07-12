fbe5601c7dba2b8656436269842c7367066914c5
terminalzzz <zliu.msb@gmail.com>
2017-07-11 22:01:14 -0400

    my commit

--- Thermometer/House.swift
+++ Thermometer/House.swift
@@ -9,4 +9,14 @@
 import Foundation
 
 
-
+class House {
+    let thermometer: Thermometer
+    
+    init () {
+        self.thermometer = Thermometer(fahrenheit: 75.0)
+    }
+    
+    func grandmaIsOver() {
+        self.thermometer.fahrenheit = 90.0
+    }
+}
