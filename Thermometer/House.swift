a6915f32785a204aa6f75f9a21a4ff0af5ececfa
Reeechi <abenor1@unlv.nevada.edu>
2017-08-08 21:21:07 -0700

    Compelted Swift Classes Lab

--- Thermometer/House.swift
+++ Thermometer/House.swift
@@ -8,5 +8,11 @@
 
 import Foundation
 
+class House {
+    let thermometer: Thermometer = Thermometer.init(fahrenheit: 75.0)
 
+    func grandmaIsOver() {
+        thermometer.fahrenheit = 90.0
+    }
+}
 
