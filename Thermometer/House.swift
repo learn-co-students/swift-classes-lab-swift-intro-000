2a2ac6c1bc765e69a4c53cb9849d764ce52f3f0d
Lenny <lenny@Lennys-MacBook-Pro.local>
2017-09-05 13:21:05 -0400

    done

--- Thermometer/House.swift
+++ Thermometer/House.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
+class House {
+    let thermometer:Thermometer = Thermometer(fahrenheit: 75)
+    
+    func grandmaIsOver()   {
+        thermometer.fahrenheit = 90.0
+    }
+}
 
 
