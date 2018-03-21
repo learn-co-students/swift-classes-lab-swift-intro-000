fb508850b5412c09bbd60492e4df4af7efd6901f
ykiimm <ykiimm@yahoo.com>
2018-03-21 14:21:19 +0900

    done

--- Thermometer/Thermometer.swift
+++ Thermometer/Thermometer.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-
-
+class Thermometer: NSObject {
+    
+    // MARK: - Property
+    
+    var fahrenheit: Double
+    var celsius: Double {
+        get {
+            return (fahrenheit - 32) * 5/9
+        }
+        set {
+            fahrenheit = newValue * 9/5 + 32
+        }
+    }
+    override var description: String {
+        get {
+            return "Fahrenheit: \(fahrenheit)\nCelsius: \(celsius)"
+        }
+        set {
+            
+        }
+    }
+    
+    
+    // MARK: - Init
+    
+    init(fahrenheit: Double) {
+        self.fahrenheit = fahrenheit
+    }
+}
